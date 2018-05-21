84ffc1aa396416fb926c04f53571114e981a2561
Chintan Patel <chintanpatel911@gmail.com>
2018-05-21 09:37:56 -0400

    Initial commit

---
+++ app/src/main/java/com/example/chintanpatel/tableview/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.chintanpatel.tableview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.framelayout_example);
+    }
+}
